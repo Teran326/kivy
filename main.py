ecec32ac80ad2e4cda70f93dd374e65fd7581c3b
Teran326 <swysog@gmail.com>
2021-02-16 13:27:32 +0100

    Image

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
+        img = Image(source='418977.jpg',
+                      size_hint=(1, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
-        return label
+        return img
 
 
 if __name__ == '__main__':
