92e2c7b15564c6847c72d84f51c574bab3ab49ad
Teran326 <swysog@gmail.com>
2021-02-16 13:41:51 +0100

    Click it!

--- main.py
+++ main.py
@@ -3,27 +3,20 @@ import random
 
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
 
 red = [1,0,0,1]
 green = [0,1,0,1]
 blue = [0,0,1,1]
 purple = [1,0,1,1]
 
-class HBoxLayoutExample(App):
+class ButtonApp(App):
     def build(self):
-        layout = BoxLayout(padding=10)
-        colors = [red, green, blue, purple]
+        return Button()
 
-        for i in range(5):
-            btn = Button(text="Button #%s" % (i+1),
-                       background_color=random.choice(colors)
-                       )
-
-            layout.add_widget((btn))
-        return layout
+    def on_press_button(self):
+        print('You pressed the button!')
 
 
 if __name__ == '__main__':
-    app = HBoxLayoutExample()
+    app = ButtonApp()
     app.run()
